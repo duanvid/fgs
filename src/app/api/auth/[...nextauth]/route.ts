7569f748a5aaf6a774c99bfed5e3d57f948d4cb8
Duan <duw4ng@gmail.com>
2024-01-05 21:17:45 +0800

    add next auth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth/next";
+import Google from "next-auth/providers/google";
+
+const handler = NextAuth({
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    })
+  ]
+})
+
+export { handler as GET, handler as POST }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,46 @@
 import Box from '@/sharedcomponents/Box'
 import Button from '@/sharedcomponents/Button'
-import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col">
       <Box>
-        <nav className='flex flex-row justify-between items-center'>
-        <div className='text-2xl font-semibold'>Fotografer Samarinda</div>
-        <div className='flex-row flex items-center space-x-5'>
-          <ul className='flex flex-row space-x-2'>
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Gallery</li>
-            <li>Contact Us</li>
-          </ul>
-          <div>
-            <Button text='Login or Register' />
+        <nav className='flex flex-row justify-between items-center p-5'>
+          <div className='text-2xl font-semibold'>Fotografer Samarinda</div>
+          <div className='flex-row flex items-center space-x-5'>
+            <ul className='lg:flex flex-row space-x-2 hidden'>
+              <li>Home</li>
+              <li>About Us</li>
+              <li>Gallery</li>
+              <li>Contact Us</li>
+            </ul>
+            <div>
+              <Button text='Login or Register' />
+            </div>
           </div>
-        </div>
-      </nav>
+        </nav>
       </Box>
-        <section className='h-5/6 p-5 container'>Hero Section</section>
+        <section className='bg-emerald-500 min-h-96'>
+          <Box>
+            <div className="section__content flex flex-col justify-evenly">
+              <div className="p-5">
+                <h1 className="text-4xl font-semibold text-white">Temukan Fotografer di samarinda hanya dengan sekali klik</h1>
+              </div>
+              <div className="p-5 text-white space-y-5">
+                <div>
+                  <p>Kami adalah platform yang menyediakan wadah bagi bakat fotografi Anda untuk berkembang.</p>
+                  <p>Dari pernikahan (wedding photography) hingga sesi potret pribadi,</p>
+                  <p>Anda dapat mengejar minat Anda dan berbagi keahlian Anda dengan klien yang mencarinya.</p>
+                </div>
+                <div className="flex flex-wrap gap-2">
+                  <Link href='/discover'><button className="border py-2 px-4 rounded">Jelajahi Fotografer</button></Link>
+                  <button className={`border py-2 px-4 rounded`}>Daftar Sebagai Fotografer</button>
+                </div>
+              </div>
+            </div>
+          </Box>
+        </section>
 
         <section className='bg-sky-500'>
           <Box>
